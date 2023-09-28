f38c5793e73c51b932d82453a02999e34f39ff14
KrystianPi <krystek.pietrzak@gmail.com>
2023-09-28 22:05:26 +0200

    queries to create a table with rome geo tags and ingested items

---
+++ sql_queries/create_rome_geo_tags.sql
@@ -0,0 +1,6 @@
+CREATE TABLE rome_geo_tags (
+    gt_id int unsigned,
+    gt_page_id int unsigned,
+    gt_lat decimal(11,8),
+    gt_lon decimal(11,8)
+);
--- sql_queries/rome_wiki_geo_tags.sql
+++ sql_queries/rome_wiki_geo_tags.sql
@@ -1,3 +1,9 @@
-select * from wiki_geo.geo_tags 
+INSERT INTO rome_geo_tags
+select 
+gt_id,
+gt_lat,
+gt_lon,
+gt_page_id
+from wiki_geo.geo_tags 
 where gt_lat > 41.85 and gt_lat < 41.95 
 and gt_lon > 12.42 and gt_lon < 12.55;
